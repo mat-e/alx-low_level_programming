a2ddf9ebacca5d398eba379020743f865db1adb7
Matome <evansmatome8@gmail.com>
2023-02-16 19:56:52 -0800

    commm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sentence[];
+	sentence* = "Programming is like building a multilingual puzze";
+	puts(sentence);
+        return (0);
+}
+
