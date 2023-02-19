cca575444f227c2c10c757281636afeba568787d
Matome <evansmatome8@gmail.com>
2023-02-19 03:08:46 -0800

    printf commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("Size of char: ",sizeof(char)+"\n");
-	printf("Size of an int: ",sizeof(int)+"\n");
-	printf("Size of a long int: ",sizeof(long int)+"\n");
-	printf("Size of a long long int: ",sizeof(long long int)+"\n");
-	printf("Size of a float: ",sizeof(float));
+	printf("Size of char: ",sizeof(char)+"byte(s)\n");
+	printf("Size of an int: ",sizeof(int)+"byte(s)\n");
+	printf("Size of a long int: ",sizeof(long int)+"byte(s)\n");
+	printf("Size of a long long int: ",sizeof(long long int)+"byte(s)\n");
+	printf("Size of a float: ",sizeof(float)+"byte(s)");
+	return(0);
 }	
