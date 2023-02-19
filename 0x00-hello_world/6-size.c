ea08833ed44801e647c5a27348669f9032df893e
Matome <evansmatome8@gmail.com>
2023-02-19 04:04:55 -0800

    see commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
@@ -12,4 +13,5 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
+
 }	
