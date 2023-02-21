2ad9e12ab65844e2a3d2c1860d64b630fcc22663
Matome <evansmatome8@gmail.com>
2023-02-21 12:26:24 -0800

    alphabet print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+int main()
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
