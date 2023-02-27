857d96133ce7933187c41998357064c51c757cb1
Matome <evansmatome8@gmail.com>
2023-02-26 22:03:08 -0800

    resend commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,10 +6,11 @@
 Return : Always success
  * */
 void reset_to_98(int *n){
+	int *n;
 	*n = 98;
 }
 
-int main(){
+int main(void){
 	int n;
 	n = 402;
 	printf("n = %d\n", n);
@@ -17,3 +18,4 @@ int main(){
 	printf("n = %d\n", n);
 	return (0);
 }
+
