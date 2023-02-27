6e5bbe8291a48a9005d99ed976b37e6574aa6df3
Matome <evansmatome8@gmail.com>
2023-02-26 22:27:15 -0800

    re recommit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,16 +6,5 @@
 Return : Always success
  * */
 void reset_to_98(int *n){
-	int *n;
 	*n = 98;
 }
-
-int main(void){
-	int n;
-	n = 402;
-	printf("n = %d\n", n);
-	reset_to_98(n);
-	printf("n = %d\n", n);
-	return (0);
-}
-
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main
-#define main
+#ifndef main_h
+#define main_h
 
 	void reset_to_98(int *n);
 
