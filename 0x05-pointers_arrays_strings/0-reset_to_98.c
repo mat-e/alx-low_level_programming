b25dfd9394bd1e223c401e8b95a0bb44f68300a9
Matome <evansmatome8@gmail.com>
2023-02-26 22:30:05 -0800

    re recommit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 /*
- *main function to update a value
+ *reset_to_98() function to update a value
 Return : Always success
- * */
-void reset_to_98(int *n){
+*/
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
+
