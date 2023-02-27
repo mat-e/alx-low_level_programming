23a4193c3d2b7c124dec45bbbe81effd0f7df019
Matome <evansmatome8@gmail.com>
2023-02-26 22:34:08 -0800

    re recommit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,7 @@ Return : Always success
 */
 void reset_to_98(int *n)
 {
-	// reset_to_98 fucntions changes the value at address to 98
+	/* reset_to_98 fucntions changes the value at address to 98*/
 	*n = 98;
 }
 
